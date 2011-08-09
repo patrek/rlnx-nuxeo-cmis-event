c60ae977e49a2092eef0add654e956d9a4c23adf
patrek <patrek@revolutionlinux.com>
2011-08-09 13:46:31 -0400

    Simplification du log.

--- src/main/java/com/rlnx/nuxeo/cmis/CmisActionListener.java
+++ src/main/java/com/rlnx/nuxeo/cmis/CmisActionListener.java
@@ -23,38 +23,38 @@ public class CmisActionListener implements EventListener {
 			DocumentModel document = ((DocumentEventContext) ctx)
 					.getSourceDocument();
 			if (null != document) {
-				log.debug("Status before : document "
-						+ (document.isCheckedOut() ? "checkedout "
-								: "checkedin ")
-						+ (document.isLocked() ? "locked" : "unlocked"));
+				if (log.isDebugEnabled()) {
+					log.debug("Status before : document "
+							+ (document.isCheckedOut() ? "checkedout "
+									: "checkedin ")
+							+ (document.isLocked() ? "locked" : "unlocked"));
+				}
 				if (DocumentEventTypes.DOCUMENT_LOCKED.equals(eventName)) {
-					log.debug("Time to checkout document");
 					if (!document.isCheckedOut()) {
 						document.checkOut();
 					}
 				} else if (DocumentEventTypes.DOCUMENT_UNLOCKED
 						.equals(eventName)) {
-					log.debug("Time to checkin document");
 					if (document.isCheckedOut()) {
 						document.checkIn(VersioningOption.NONE, "unlocked");
 					}
 				} else if (DocumentEventTypes.DOCUMENT_CHECKEDIN
 						.equals(eventName)) {
-					log.debug("Time to unlock document");
 					if (document.isLocked()) {
 						document.removeLock();
 					}
 				} else if (DocumentEventTypes.DOCUMENT_CHECKEDOUT
 						.equals(eventName)) {
-					log.debug("Time to lock document");
 					if (!document.isLocked()) {
 						document.setLock();
 					}
 				}
-				log.debug("Status after : document "
-						+ (document.isCheckedOut() ? "checkedout "
-								: "checkedin ")
-						+ (document.isLocked() ? "locked" : "unlocked"));
+				if (log.isDebugEnabled()) {
+					log.debug("Status after : document "
+							+ (document.isCheckedOut() ? "checkedout "
+									: "checkedin ")
+							+ (document.isLocked() ? "locked" : "unlocked"));
+				}
 			}
 		}
 	}
